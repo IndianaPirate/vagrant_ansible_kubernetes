64d13294c2dd445eec599c415da38c7b5199547b
NIAINA Andriharisoa <niaina-akata@goavana.com>
2023-08-16 14:43:52 +0200

    Add configuration

---
+++ creds.sh
@@ -0,0 +1,14 @@
+echo "Active l'authentification par mot de passe"
+sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
+systemctl reload sshd
+
+echo "Mettre le mot de passe root"
+echo -e "kubeadmin\nkubeadmin" | passwd root >/dev/null 2>&1
+echo "export TERM=xterm" >> /etc/bash.bashrc
+
+apt update -y 
+apt install ansible -y
+apt install git -y
+git clone https://github.com/IndianaPirate/vagrant_ansible_kubernetes.git 
+ansible-galaxy playbook_node.yml
