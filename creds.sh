3dee7688d840142d30c9168567cefcfc7643a63b
NIAINA Andriharisoa <niaina-akata@goavana.com>
2023-08-16 16:38:14 +0200

    fix bug

--- creds.sh
+++ creds.sh
@@ -1,3 +1,11 @@
+echo "[+] Update repos"
+apt update -y 
+echo "[+] Install ansible"
+apt install ansible -y
+echo "[+] Install git"
+apt install git -y
+echo "[+] Fix broken install"
+apt --fix-broken install -y
 echo "Active l'authentification par mot de passe"
 sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
 echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
@@ -7,9 +15,8 @@ echo "Mettre le mot de passe root"
 echo -e "kubeadmin\nkubeadmin" | passwd root >/dev/null 2>&1
 echo "export TERM=xterm" >> /etc/bash.bashrc
 
-apt update -y 
-apt install ansible -y
-apt install git -y
+echo "[+] Clone repository"
 git clone https://github.com/IndianaPirate/vagrant_ansible_kubernetes.git
 cd vagrant_ansible_kubernetes
+echo "[+] Ansible Galaxy"
 ansible-galaxy playbook_node.yml -i inventory
